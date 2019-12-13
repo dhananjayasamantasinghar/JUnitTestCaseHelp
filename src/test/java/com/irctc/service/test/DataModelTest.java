3b7bcaf843cbe41899c85eb25a7ff6ef6d1b9830
dhananjayasamantasinghar <52064780+dhananjayasamantasinghar@users.noreply.github.com>
2019-12-13 15:45:17 +0530

    bean matcher test

---
+++ src/test/java/com/irctc/service/test/DataModelTest.java
@@ -0,0 +1,18 @@
+package com.santander.olb.common.rsa.score.model;
+
+import org.junit.Test;
+
+public class DataModelTest {
+    @Test
+    public void testAuthorizeRequest() {
+        //assertThat(ImgPhData.class, hasValidBeanConstructor());
+        //assertThat(ImgPhData.class, hasValidGettersAndSetters());
+
+        //assertThat(RsaDeviceAnalysisDefaultResponse.class, hasValidBeanConstructor());
+       // assertThat(RsaDeviceAnalysisDefaultResponse.class, hasValidGettersAndSetters());
+
+        //assertThat(Question.class, hasValidBeanConstructor());
+       // assertThat(Question.class, hasValidGettersAndSetters());
+    }
+
+}
